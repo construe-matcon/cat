0e0d554fbbfac28c27817a50f9dc532d0f96b7ac
Eduardo F. Dutra <eduardo.dutra@voidstdo.com>
2018-12-20 16:58:15 -0200

    Rota Login

--- src/router.js
+++ src/router.js
@@ -9,14 +9,14 @@ export default new Router({
     mode: 'history',
     base: process.env.BASE_URL,
     routes: [
+        // {
+        //     path: '/',
+        //     redirect: {
+        //         name: "login"
+        //     }
+        // },
         {
-            path: '/',
-            redirect: {
-                name: "login"
-            }
-        },
-        {
-            path: "/login",
+            path: "/",
             name: "login",
             component: LoginComponent
         },
