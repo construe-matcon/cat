1cc9ad001f900cc704218f64a82ac8029afd952e
Eduardo F. Dutra <eduardo.dutra@voidstdo.com>
2019-01-03 16:10:04 -0200

    mode: hash

--- src/Routes.js
+++ src/Routes.js
@@ -27,7 +27,7 @@ import NotificationsPage from '@/pages/Notifications/Notifications';
 Vue.use(Router);
 
 export default new Router({
-  mode: 'history',
+  mode: 'hash',
   base: process.env.BASE_URL,
   routes: [
     {
