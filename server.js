46efc685f19ee592fe23e51aedc320308925bef2
Eduardo F. Dutra <eduardo.dutra@voidstdo.com>
2019-01-03 15:26:48 -0200

    Server.js bugfix

--- server.js
+++ server.js
@@ -3,11 +3,10 @@ var serveStatic = require('serve-static');
 var history = require('connect-history-api-fallback');
 
 var app = express();
-app.use(serveStatic(__dirname + "/dist"));
-
-app.use(history({
-  index: '/'
-}));
+app.use(
+	serveStatic(__dirname + "/dist"),
+	history({index: '/'})
+	);
 
 var port = process.env.PORT || 5000;
 app.listen(port);
