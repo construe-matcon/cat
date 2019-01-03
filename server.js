ad9e088712c856989a4da1c4a7171d48d0023a3e
Eduardo F. Dutra <eduardo.dutra@voidstdo.com>
2019-01-03 15:22:16 -0200

    History API Node

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var serveStatic = require('serve-static');
+var history = require('connect-history-api-fallback');
 
 var app = express();
 app.use(serveStatic(__dirname + "/dist"));
+
+app.use(history({
+  index: '/'
+}));
 
 var port = process.env.PORT || 5000;
 app.listen(port);
