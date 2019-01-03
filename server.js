64ea138f99f3d2f52c31db3a0e81be790ce6d604
Eduardo F. Dutra <eduardo.dutra@voidstdo.com>
2019-01-03 15:59:20 -0200

    Remove History

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
 var express = require('express');
 var serveStatic = require('serve-static');
-var history = require('connect-history-api-fallback');
 
 var app = express();
-app.use(
-	serveStatic(__dirname + "/dist"),
-	history({index: '/'})
-	);
+app.use(serveStatic(__dirname + "/dist"));
 
-var port = process.env.PORT || 5000;
+
+var port = process.env.PORT || 5050;
 app.listen(port);
