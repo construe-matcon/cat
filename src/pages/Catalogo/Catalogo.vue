a3a724f4531feca9d4838df99f42e6fd8da7079b
Rafael Lazarini <rlazarini@majmun.com.br>
2019-02-14 10:26:03 -0200

    tela de catalogo

--- src/pages/Catalogo/Catalogo.vue
+++ src/pages/Catalogo/Catalogo.vue
@@ -1,37 +1,91 @@
 <template>
 	<div v-if="idCatalogo">
-		<h1 class="page-title">Catálogo - {{listCatalogo[0].industria}}</h1>
-		<h5 class="page-title"><small>Última atualização: <span class='fw-semi-bold'>{{idCatalogo}}</span></small></h5>
-
-		<b-row>
-			<b-col class="min">
-				<div class="pb-xlg h-100">
-					<Widget class="h-100 mb-0" title="Produtos Sem Categoria">
-						<b-row>
-							<canvas id="piechart0" height="350"></canvas>
-						</b-row>
+		<template v-if="listCatalogo.length > 0">
+			<h1 class="page-title">Catálogo - {{listCatalogo[0].industria}}</h1>
+			<h5 class="page-title"><small>Última atualização: <span class='fw-semi-bold'>{{idCatalogo}}</span></small></h5>
+			<b-row>
+				<b-col>
+					<Widget>
+						<div class="table-resposive table-hover">
+							<table class="table">
+								<thead>
+									<tr>
+										<th class="hidden-sm-down">ID</th>
+										<th class="hidden-sm-down">Códigos</th>
+										<th>Imagem</th>
+										<th>Nome</th>
+										<th class="hidden-sm-down">Linha</th>
+										<th class="hidden-sm-down">Marca</th>
+										<th class="hidden-sm-down">Categoria</th>
+										<th class="hidden-sm-down">Tags</th>
+									</tr>
+								</thead>
+								<tbody>
+									<tr v-for="row in listCatalogo" :key="row.id" @click="ir(row.id)">
+										<td>{{row.id}}</td>
+										<td>
+											<p class="mb-0">
+												<small>
+													<span class="fw-semi-bold">EAN:</span>
+													<span class="text-muted">&nbsp; {{row.ean}}</span>
+												</small>
+											</p>
+											<p>
+												<small>
+													<span class="fw-semi-bold">NCM:</span>
+													<span class="text-muted">&nbsp; {{row.ncm}}</span>
+												</small>
+											</p>
+										</td>
+										<td>
+											<img class="img-rounded imgCat" :src="'https://images.construe.cf/'+row.industria+'/'+row.ean+'.jpg'" alt="" />
+										</td>
+										<td>
+											{{row.descricao}}
+											<div v-if="row.label">
+												<b-badge :variant="row.label.colorClass">{{row.label.text}}</b-badge>
+											</div>
+										</td>
+										<td class="text-semi-muted">
+											{{row.linha}}
+										</td>
+										<td class="text-semi-muted">
+											{{row.marca}}
+										</td>
+										<td class="text-semi-muted">
+											{{row.categoria}}
+										</td>
+										<td class="width-150">
+											<template v-if="row.tags">
+												<template v-if="row.tags.length > 0">
+													<span v-for="(tags, i) in row.tags" :key="i">
+														{{tags}}<br>
+													</span>
+												</template>
+											</template>
+										</td>
+									</tr>
+								</tbody>
+							</table>
+						</div>
+						<b-pagination v-if="tamanho <= totalProdutos"
+							align="right"
+							size="md"
+							next-text="Proximo"
+							prev-text="Anterior"
+							:limit="10"
+							:hide-goto-end-buttons="true"
+							:total-rows="totalProdutos"
+							:per-page="tamanho"
+							@input="mudaPagina"
+						/>
 					</Widget>
-				</div>
-			</b-col>
-			<b-col class="min">
-				<div class="pb-xlg h-100">
-					<Widget class="h-100 mb-0" title="Produtos Sem Tags">
-						<b-row>
-							<canvas id="piechart1" height="350"></canvas>
-						</b-row>
-					</Widget>
-				</div>
-			</b-col>
-			<b-col class="min">
-				<div class="pb-xlg h-100">
-					<Widget class="h-100 mb-0" title="Produtos Sem EAN">
-						<b-row>
-							<canvas id="piechart2" height="350"></canvas>
-						</b-row>
-					</Widget>
-				</div>
-			</b-col>
-		</b-row>
+				</b-col>
+			</b-row>
+		</template>
+		<template v-else>
+			Dados não encontrados
+		</template>
 	</div>
 	<div v-else>
 		<h1 class="page-title">Catálogos</h1>
@@ -63,6 +117,10 @@
 		components: { Widget },
 		data() {
 			return {
+				totalProdutos: 0,
+				tamanho: 1,
+				paginaAtual: 0,
+				ultimaPagina: true,
 				listCatalogo: [],
 				idCatalogo: this.$route.params.id,
 				listImgCatalogo: {},
@@ -81,7 +139,10 @@
 			},
 			loadCat(obj) {
 				this.listCatalogo = obj.data
-				console.log(obj.data)
+				this.totalProdutos = obj.total_data_size;
+				this.tamanho = obj.size;
+				this.paginaAtual = obj.number;
+				this.ultimaPagina = obj.last_page;
 			},
 			goToCat(id) {
 				this.$router.push({
