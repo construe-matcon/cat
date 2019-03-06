934eaf2703acbefdaa6acb3be32e2eeeb6bfcac6
Rafael Lazarini <rlazarini@majmun.com.br>
2019-03-06 14:05:51 -0300

    cadastro de usuario parassando perfil permissao

--- src/pages/_CadUser/_CadUser.vue
+++ src/pages/_CadUser/_CadUser.vue
@@ -15,6 +15,30 @@
 						<input class="form-control no-border" v-model="dados.senha" ref="password"
 						required type="password" name="password" placeholder="Senha" />
 					</div>
+					<div class="form-group">
+						<b-form-group label="Perfil" label-cols="6" label-for="perfil">
+							<b-form-radio-group
+								id="perfil"
+								buttons
+								button-variant="primary"
+								v-model="dados.id_perfil"
+								:options="perfilOpt"
+								name="radiosBtnDefault"
+							/>
+						</b-form-group>
+					</div>
+					<div class="form-group">
+						<b-form-group label="Associar Industria" label-cols="6" label-for="industria">
+							<b-form-checkbox class="float-left" v-model="dados.ind_todas_industrias" @change="toggleAll">Todas</b-form-checkbox>
+							<b-form-checkbox-group
+								id="industria1"
+								name="indBasico1"
+								v-model="dados.ids_industrias"
+								:options="industriaOpt"
+								@change="uncheckAll"
+							/>
+						</b-form-group>
+					</div>
 					<b-alert class="alert-sm" variant="danger" :show="!!errorMessage">
 						<span v-html="errorMessage"></span>
 					</b-alert>
@@ -49,29 +73,38 @@
 					'email': '',
 					'nome': '',
 					'senha': '',
-					'id_perfil': 1,
-					'ind_todas_industrias': true,
-					'ids_industrias': ''
+					'id_perfil': '',
+					'ind_todas_industrias': false,
+					'ids_industrias': []
 				},
+				perfilOpt: [
+					{ text: 'Admin', value: 1 },
+					{ text: 'Visualizar', value: 2 },
+					{ text: 'Industria', value: 3 }
+				],
+				industriaOpt: [],
 				errorMessage: '',
 				successMessage: '',
 			};
 		},
 		methods: {
 			async cadastro(){
-				var email = this.dados.email
+				let dados = JSON.parse(JSON.stringify(this.dados))
+				if (dados.ind_todas_industrias) {
+					dados.ids_industrias = []
+				}
+				var email = dados.email
 				this.errorMessage = ''
 				this.successMessage = ''
 
 				if (email.split('@')[1].indexOf('construe') >= 0 || email.split('@')[1].indexOf('yandeh') >= 0) {
-					await gfn.fApi({url:"https://api.construe.cf/usuarios", options: {method: 'POST', body: JSON.stringify(this.dados)}}, this.sendData);
+					await gfn.fApi({url:"https://api.construe.cf/usuarios", options: {method: 'POST', body: JSON.stringify(dados)}}, this.sendData);
 				} else {
 					this.errorMessage = 'O email precisa ser Yandeh ou Construe para poder efetuar o cadastro'
 				}
 			},
 			sendData(obj){
 				if(obj.mensagens) {
-					console.log(obj)
 					this.errorMessage = ''
 
 					for (var i = 0, lgt = obj.mensagens.length; i < lgt; i++ ) {
@@ -86,7 +119,26 @@
 					}
 				}
 			},
+			toggleAll(checked) {
+				this.dados.ids_industrias = checked ? this.industriaOpt.map(opt => opt.value) : []
+			},
+			uncheckAll(checked) {
+				if (this.industriaOpt.length == checked.length) {
+					this.dados.ind_todas_industrias = true
+				} else {
+					this.dados.ind_todas_industrias = false
+				}
+			}
 		},
+		async mounted() {
+			let allInd = await gfn.fApi({url:"https://api.construe.cf/industrias?tamanho_pagina=200", options: {method: 'GET'}});
+			allInd.data.forEach( ind => {
+				this.industriaOpt.push({
+					'text': ind.nome,
+					'value': ind.id
+				})
+			});
+		}
 	};
 </script>
 
